34a7b4e9f5c9e1c50c1321f0b44e42a754ce9c20
AkshayV30 <akshayatwork.v30@gmail.com>
2023-02-09 13:20:25 +0530

    final

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const express   = require ('express');
 const redis = require ('redis');
-const process = require('process');  //to deliberately crashing the server
+// const process = require('process');  //to deliberately crashing the server
 
 const app = express();
 const client = redis.createClient({
     host: 'redis-server' ,
     port: 6379
 });
-client.set('visits', 0);
+client.set('count', 0);
 
 app.get('/', (req, res ) => {
-    process.exit(0);   //exit status code 0 to close the process
-    client.get('visits', (err, visits) => {
-        res.send('Number of Visits : ' + visits);
-        client.set('visits', parseInt(visits) + 1);
+    // process.exit(0);   //exit status code 0 to close the process
+    client.get('count', (err, count) => {
+        res.send('Number of visit : ' + count);
+        client.set('count', parseInt(count) + 1);
     });
 });
 
 
-app.listen(83, () => {
+app.listen(8081, () => {
     console.log('listening here');
 });
 
